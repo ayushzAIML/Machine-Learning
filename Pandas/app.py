96d4443fc777392c5ff1a43e62cd1e09b27a4f0f
Ayush Chakraborty <theayushchakraborty@gmail.com>
2025-09-01 20:30:14 +0530

    Initialize app.py with pandas import and print

---
+++ Pandas/app.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# df = pd.read_csv('sales_data_sample.csv', encoding='latin1')
+# df = pd.read_excel('SampleSuperstore.xlsx')
+# df = pd.read_json('/home/ayushz/ML/pandas/sample_Data.json')
+
+print(df)
